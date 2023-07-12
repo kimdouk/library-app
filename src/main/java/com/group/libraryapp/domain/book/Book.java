ec88934e476c48ae3ce58d530ad7c570a3698786
KimDoUk <zjaxj230@naver.com>
2023-07-12 14:06:32 +0900

    feat: 대출, 반납 기능 추가

--- src/main/java/com/group/libraryapp/domain/book/Book.java
+++ src/main/java/com/group/libraryapp/domain/book/Book.java
@@ -12,10 +12,22 @@ public class Book {
     @Column(nullable = false, length = 255, name = "name")
     private String name;
 
+    protected Book() {
+
+    }
+
     public Book(String name) {
         if (name == null || name.isBlank()) {
             throw new IllegalArgumentException(String.format("잘못된 name(%s)이 들어왔습니다", name));
         }
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
